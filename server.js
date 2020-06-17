f6f7ed72ee9cac5c724092d06008f205ad0a861d
Shayda Bakhshi <shayda.bakhshi@gmail.com>
2020-06-16 21:34:11 -0500

    seed file updates; update port numbers

--- server.js
+++ server.js
@@ -5,37 +5,36 @@ const mongoose = require("mongoose");
 // SHAYDA NOTE: I've commented out routes for now since none are yet included. Will update as needed.
 // const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Routes
-app.use("/api/entry",require("./routes/apiEntry"));
-
+app.use("/api/entry", require("./routes/apiEntry"));
 
 //Serve static folder
-if (process.env.NODE_ENV ==="production"){
-    app.use(express.static("src/build"))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("src/build"));
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname, "src", "build", "index.html"))
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "src", "build", "index.html"));
+  });
 }
-
 
 // Connect to the Mongo DB
 
-const MONGODB_URI=process.env.MONGODB_URI || "mongodb//localhost/plumbumApp";
-mongoose.connect(MONGODB_URI,{
-    useNewUrlParser: true,
-    useCreateIndex:true,
-    useUnifiedTopology:true,})
-    {
-        console.log("DB Connected");
-    }
-    
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb//localhost/plumbumApp";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+{
+  console.log("DB Connected");
+}
+
 // Start the API server
 app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
