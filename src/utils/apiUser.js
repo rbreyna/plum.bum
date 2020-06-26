053f01b6d8b2ab0831b4fb2417829ead895a18a4
Roy Reyna <royreyna7@yahoo.com>
2020-06-25 20:50:37 -0500

    remove user folder, since dashboard is hardcorded and updated apiUser files

--- src/utils/apiUser.js
+++ src/utils/apiUser.js
@@ -2,11 +2,16 @@ import axios from "axios";
 import useAuth0 from "../context/autho0-context";
 
 export default {
+  // Updates an entry
+  findUser: function (id) {
+    return axios.get("/api/user/" + id);
+  },
   // Saves a user to the database
   createUser: function (userData) {
     return axios.post("/api/user/", userData);
-  },  // Updates an entry
-  findUser: function (entryData, id) {
-    return axios.get("/api/entry/" + id, entryData);
   },
+  // Deletes the entry with the given id
+  deleteEntry: function (id) {
+    return axios.delete("/api/user/" + id);
+  }
 };
