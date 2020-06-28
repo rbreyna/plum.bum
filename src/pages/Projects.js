0d5287bdc12a5434599bad78a8ce53d85f2063dd
Shayda Bakhshi <shayda.bakhshi@gmail.com>
2020-06-27 19:26:29 -0500

    prepping for merge

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,13 +9,19 @@ function Dashboard() {
 
   return (
     <div>
-      <Jumbotron className="Projects-tron">
-        <h1> {user ? `${user.name}'s` : null} Projects</h1>
-      </Jumbotron>
-
       <Container>
         <Row className="justify-center-content fluid" id="row-1">
-          <Col className="content dash1" sm={12}></Col>
+          <Col className="content dash1" sm={12}>
+            <h1> {user ? `${user.name}'s` : null} Projects</h1>
+            <Image
+              src="../assets/images/avatar1.png"
+              width={150}
+              height={150}
+              style={{ marginTop: "20px" }}
+              roundedCircle
+            />
+            <h6>{user ? `${user.name}` : null}</h6>
+          </Col>
         </Row>
         <Row className="fluid" id="row-2">
           <Col className="content dash3" sm={12}>
