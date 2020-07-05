5aaa163b52f6345c558b65b26fb3bdca1e0c295d
Shayda Bakhshi <shayda.bakhshi@gmail.com>
2020-07-04 22:10:38 -0500

    migrated portions of nav to material UI; put user pages in menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 import { useAuth0 } from "../contexts/auth0-context";
 
 function Navigation() {
   const { isLoading, user, loginWithRedirect, logout } = useAuth0();
   const picture = user ? user.picture : null;
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Navbar bg="light" expand="lg">
@@ -32,12 +46,39 @@ function Navigation() {
           {!isLoading && user && (
             <>
               <Navbar.Text>Hello {user.name}!</Navbar.Text>
-              &nbsp; &nbsp; &nbsp;
-              <Avatar alt="profile picture" src={picture} />
-              &nbsp; &nbsp; &nbsp;
-              <Nav.Link href="/profile">Profile</Nav.Link>
+              &nbsp;
+              <Button
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                <Avatar alt="profile picture" src={picture} />
+              </Button>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem component={Link} to="/profile" onClick={handleClose}>
+                  Profile
+                </MenuItem>
+                <MenuItem
+                  component={Link}
+                  to="/dashboard"
+                  onClick={handleClose}
+                >
+                  Dashboard
+                </MenuItem>
+                <MenuItem component={Link} to="/projects" onClick={handleClose}>
+                  My Projects
+                </MenuItem>
+              </Menu>
+              &nbsp;
+              {/* <Nav.Link href="/profile">Profile</Nav.Link>
               <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-              <Nav.Link href="/projects">My Projects</Nav.Link>
+              <Nav.Link href="/projects">My Projects</Nav.Link> */}
               <Nav.Link
                 onClick={() =>
                   logout(
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -15,10 +15,17 @@ import Container from "@material-ui/core/Container";
 
 // Material UI variables
 const useStyles = makeStyles((theme) => ({
-  large: {
-    width: theme.spacing(7),
-    height: theme.spacing(7),
+  avatar: {
+    width: theme.spacing(10),
+    height: theme.spacing(10),
     marginTop: "20px",
+  },
+  userHeader: {
+    justify: "space-between",
+    wrap: "nowrap",
+  },
+  headerText: {
+    marginLeft: "20px",
   },
   root: {
     flexGrow: 1,
@@ -34,34 +41,33 @@ function Dashboard() {
   const { user } = useAuth0();
   const picture = user ? user.picture : null;
   const classes = useStyles();
+  const name = user ? user.name : null;
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <div className={classes.root}>
-          <Grid container spacing={3}>
-            <Grid item xs={3}>
-              <Paper className={classes.paper}>
-                <Avatar
-                  alt="user image"
-                  src={picture}
-                  className={classes.large}
-                />
-              </Paper>
-            </Grid>
-            <Grid item xs={9}>
-              <Paper className={classes.paper}>
-                <h3>{user ? `${user.name}'s` : null} Projects</h3>
-                <h6>{user ? `${user.name}` : null}</h6>
-              </Paper>
-            </Grid>
-            <Grid item xs={12}>
-              <Paper className={classes.paper}>xs=12</Paper>
-            </Grid>
+    <>
+      <div className={classes.userHeader}>
+        <Container>
+          <Grid item xs={12}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <img alt={name} src={picture} className={classes.avatar} />
+              <h1 className={classes.headerText}>
+                {user ? `${user.name}'s` : null} Projects
+              </h1>
+            </div>
           </Grid>
-        </div>
+        </Container>
+      </div>
+      <Container>
+        <Grid item xs={12} style={{ marginTop: "40px" }}>
+          <PopulateEntries />
+        </Grid>
       </Container>
-    </ThemeProvider>
+    </>
   );
 }
 
