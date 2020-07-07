eb0effcc8cf25643877f84c3fe02b2a3b9c4cbbd
Shayda Bakhshi <shayda.bakhshi@gmail.com>
2020-07-06 21:37:08 -0500

    updated Save button in WordCount.js to be MUI component; functionality is working but may need more testing

---
+++ src/components/WordCount/SaveButton.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Fab from "@material-ui/core/Fab";
+import SaveIcon from "@material-ui/icons/Save";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+  fab: {
+    margin: 0,
+    top: "auto",
+    right: 30,
+    bottom: 50,
+    left: "auto",
+    position: "fixed",
+    "&:focus": {
+      outline: "none",
+    },
+  },
+}));
+
+export default function SaveButton(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <a href="/dashboard">
+        <Fab
+          id={props.id}
+          onClick={props.handleSave}
+          className={classes.fab}
+          variant="extended"
+          color="secondary"
+        >
+          <SaveIcon className={classes.extendedIcon} />
+          Save
+        </Fab>
+      </a>
+    </div>
+  );
+}
--- src/components/WordCount/WordCount.js
+++ src/components/WordCount/WordCount.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
-import { Button, Col, Row, FormControl, InputGroup, Form } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Row,
+  FormControl,
+  InputGroup,
+  Form,
+} from "react-bootstrap";
 import "../../pages/Dashboard.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faSave } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faSave } from "@fortawesome/free-solid-svg-icons";
 import apiEntry from "../../utils/apiEntry";
 import GoalReached from "../WritingGoal/GoalReached";
-
+import SaveButton from "./SaveButton";
 
 export default class WordCount extends Component {
   constructor(props) {
@@ -14,7 +21,6 @@ export default class WordCount extends Component {
       title: "",
       entryBody: "",
       wordCount: 0,
-
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -27,8 +33,7 @@ export default class WordCount extends Component {
   handleChange(event) {
     const value = event.target.value;
     this.setState({ [event.target.id]: value });
-    console.log(value)
-
+    console.log(value);
   }
 
   handleSave = (event) => {
@@ -40,23 +45,28 @@ export default class WordCount extends Component {
       entryBody: this.state.entryBody,
     };
 
-    if ((this.state.entryBody.length > 0) && (this.state.title.length > 0)) {
-
-      apiEntry.createEntry(localStorage.getItem("id"), newEntry)
-        .then(this.setState({
-          message: alert("Your pasage is saved"),
+    if (this.state.entryBody.length > 0 && this.state.title.length > 0) {
+      apiEntry.createEntry(localStorage.getItem("id"), newEntry).then(
+        this.setState({
+          message: alert("Your project has been saved!"),
           title: "",
-          entryBody: " "
-        }))
-
+          entryBody: " ",
+        })
+      );
       window.location.reload();
-
     } else {
-      this.setState({ message: alert("Must fill it out the fields") })
+      this.setState({
+        message: alert(
+          "Uh-oh, looks like you need to fill out the form completely before you can submit."
+        ),
+      });
     }
-
   };
   render() {
+    const camo = {
+      backgroundColor: "#a8e6cf",
+    };
+
     return (
       <div className="wordcount">
         <Form>
@@ -64,12 +74,9 @@ export default class WordCount extends Component {
             <Col className="content dash3" sm={12}>
               <InputGroup>
                 <br></br>
-                <InputGroup id="passage-title" sm={12} >
+                <InputGroup id="passage-title" sm={12}>
                   <InputGroup.Prepend>
-                    <InputGroup.Text
-                    >
-                      Name of Passage
-                </InputGroup.Text>
+                    <InputGroup.Text>Name of Passage</InputGroup.Text>
                   </InputGroup.Prepend>
                   <FormControl
                     id="title"
@@ -77,16 +84,17 @@ export default class WordCount extends Component {
                     name="title"
                     type="text"
                     className="form-control"
-                    onChange={this.handleChange} />
+                    onChange={this.handleChange}
+                  />
                   <br></br>
                 </InputGroup>
-                <FormControl id="entryBody"
+                <FormControl
+                  id="entryBody"
                   value={this.state.entryBody}
                   name="bodyEntry"
                   type="text"
                   className="form-control"
                   onChange={this.handleChange}
-
                   as="textarea"
                   aria-label="With textarea"
                 />
@@ -96,20 +104,23 @@ export default class WordCount extends Component {
           <Row>
             <Col sm={4}>
               <h3>
-                <FontAwesomeIcon icon={faEdit} />  : {this.countWords(this.state.entryBody)}
+                <FontAwesomeIcon icon={faEdit} /> :{" "}
+                {this.countWords(this.state.entryBody)}
               </h3>
             </Col>
             <Col sm={4}>
-              <Button id="btns"
+              <div
+                style={camo}
+                id="btns"
                 onClick={this.handleSave}
-              /*disabled={!this.state.formValid}*/
+                /*disabled={!this.state.formValid}*/
               >
-                <FontAwesomeIcon icon={faSave} />
+                <SaveButton />
                 <>
                   {/*when save btn is clicked need to goto GoalReached.js so modal pops up*/}
                   <GoalReached />
                 </>
-              </Button>
+              </div>
             </Col>
           </Row>
         </Form>
