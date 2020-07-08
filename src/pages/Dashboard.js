c44d65ae14c27f8967fb8bc3d005805466ccedfd
Roy Reyna <royreyna7@yahoo.com>
2020-07-08 02:16:49 -0500

    dashboard.js updates

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,50 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Dashboard.css";
-import { Container, Col, Row, Image } from "react-bootstrap";
+import { Container, Image } from "react-bootstrap";
 import { useAuth0 } from "../contexts/auth0-context";
 import { WritingGoal } from "../components/WritingGoal/WritingGoal.js";
 import WordCount from "../components/WordCount/WordCount";
 import apiUser from "../utils/apiUser";
 import GoalReached from "../components/WritingGoal/GoalReached.js";
 import DashboardUI from "../components/DashboardUI";
-import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-}));
-
 function Dashboard() {
-  const { isLoading, user } = useAuth0();
-  const [modalShow, setModalShow] = React.useState(false);
+  const { user } = useAuth0();
+  const [dbUser, setUser] = useState({});
 
-  const picture = user ? user.picture : null;
+  const [modalShow, setModalShow] = useState(false);
 
-  const userInfo = (id) => {
+  //Grab ID from Auth0 user to make queries to the database
+  const userID = user ? user.sub.split("|")[1] : null;
+
+  //When an ID is found, a query is made to MongoDB to grab the record for that user
+  useEffect(() => {
+    if (userID) {
+      loadUserInfo(userID);
+    }
+    return () => {};
+  }, [user, userID]);
+
+  //API call to grab record
+  const loadUserInfo = (id) => {
     apiUser
       .findUser(id)
       .then((res) => {
-        console.log("User found!");
-        console.log(res);
+        setUser({
+          name: res.data.name,
+          email: res.data.email,
+          picture: res.data.image,
+          id: res.data.auth0_id,
+        });
       })
       .catch((err) => {
         console.log(err);
       });
   };
-  const id = user ? user.sub.split("|")[1] : null;
-
-  const classes = useStyles();
-
-  // const statsDiv = {
-  //   marginBottom: "10px",
-  // };
 
   const goalDiv = {
     marginBottom: "15px",
@@ -60,44 +57,39 @@ function Dashboard() {
   return (
     <>
       <div>
-        {!isLoading && user && (
-          <>
-            {userInfo(user.sub.split("|")[1])}
-            <h1 style={headerStyles}>{user.name}'s Dashboard</h1>
-
-            <Container>
-              <div>
-                <Grid container spacing={3}>
-                  <Grid item xs={12} sm={6}>
-                    <Image
-                      src={picture}
-                      width={320}
-                      height={320}
-                      roundedCircle
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <DashboardUI />
-                  </Grid>
+        <>
+          <h1 style={headerStyles}>{dbUser.name}'s Dashboard</h1>
+          <Container>
+            <div>
+              <Grid container spacing={3}>
+                <Grid item xs={12} sm={6}>
+                  <Image
+                    src={dbUser.picture}
+                    width={320}
+                    height={320}
+                    roundedCircle
+                  />
                 </Grid>
-              </div>
-              <div style={goalDiv}>
-                <WritingGoal
-                  show={modalShow}
-                  onHide={() => setModalShow(false)}
-                />
-                <GoalReached
-                  show={modalShow}
-                  onHide={() => setModalShow(false)}
-                />
-              </div>
-              <WordCount />
-            </Container>
-          </>
-        )}
+                <Grid item xs={12} sm={6}>
+                  <DashboardUI />
+                </Grid>
+              </Grid>
+            </div>
+            <div style={goalDiv}>
+              <WritingGoal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+              />
+              <GoalReached
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+              />
+            </div>
+            <WordCount />
+          </Container>
+        </>
       </div>
     </>
   );
 }
-
 export default Dashboard;
